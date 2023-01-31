8a2022eb194910946872fea194a16b9292dbb80e
Dani <hostnameadmin@protonmail.com>
2023-01-31 10:06:55 +0100

    get, post del kesz

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Get, Post, Render } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Render } from '@nestjs/common';
 import { DataSource } from 'typeorm';
 import { AppService } from './app.service';
+import { Csavar } from './csavar.entity';
 
 @Controller()
 export class AppController {
@@ -13,5 +14,24 @@ export class AppController {
   @Render('index')
   index() {
     return { message: 'Welcome to the homepage' };
+  }
+
+  @Get('api/csavarbolt')
+  async allCsavar() {
+    const csavarboltRepo = this.dataSource.getRepository(Csavar);
+    return await csavarboltRepo.find();
+  }
+
+  @Post('api/csavarbolt')
+  newCsavar(@Body() csavar: Csavar) {
+    csavar.id = undefined;
+    const boltRepo = this.dataSource.getRepository(Csavar);
+    boltRepo.save(csavar);
+  }
+
+  @Delete('api/csavarbolt/:id')
+  deleteCsavar(@Param('id') id: number) {
+    const boltRepo = this.dataSource.getRepository(Csavar);
+    boltRepo.delete(id);
   }
 }
