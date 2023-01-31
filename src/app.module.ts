fe0cebfe35531bc7d8ce2fdd47bc39e74e2dcf3a
Dani <hostnameadmin@protonmail.com>
2023-01-31 09:56:11 +0100

    adatbazis kesz

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,17 +2,20 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Csavar } from './csavar.entity';
+import { Rendeles } from './rendeles.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: 'localhost',
+      port: 3306,
       username: 'root',
       password: '',
-      database: 'database',
+      database: 'csavarbolt',
       entities: [
-        /* List of entities here */
+        Csavar, Rendeles
       ],
       synchronize: true,
     }),
